a6fbd469c32e8208f2178d81e0cd40a9243a4fff
Yuto Takano <moa17stock@gmail.com>
2026-03-01 14:19:09 +0100

    Replace main file with a FastAPI example

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from prototype!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
